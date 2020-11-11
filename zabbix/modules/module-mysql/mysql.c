1922d938e0b113dd3c9cc6eee6960ff922639dd4
Lukas Maly <smejdil@gmail.com>
2020-11-11 07:44:35 +0100

    Author

--- zabbix/modules/module-mysql/mysql.c
+++ zabbix/modules/module-mysql/mysql.c
@@ -16,7 +16,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **
-** Author: Cao Qingshan <caoqingshan@kingsoft.com>
+** Author: Anonymous
 **
 ** Version: 1.1 Last update: 2015-12-07 12:00
 **
