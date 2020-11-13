cd1b118b8a28cd40adf62869045d5a53147970ed
Charlie Root <root@fbsd.hide.lukasmaly.net>
2020-11-13 11:19:16 +0100

    Changes

--- scripts/create_dns_records.sh
+++ scripts/create_dns_records.sh
@@ -7,11 +7,14 @@
 
 EXCLUDE=`cat exclude-hosts.txt`
 
+clear
 for i in `gcloud compute instances list | grep -v NAME | awk '{print $1";"$5}'| egrep -v "${EXCLUDE}"`;
 do 
 	NAME=`echo $i | awk 'BEGIN {FS=";"}{print $1}'`;
 	IPV4=`echo $i | awk 'BEGIN {FS=";"}{print $2}'`;
-	echo "Create DNS A record for ${NAME}:"
+	echo "Create DNS A record in domain pfsense.cz:"
 	echo "-----------------------------------------------------------------------------------------------"
-	cli4 --post name='${NAME}' type=A content="${IPV4}" /zones/:pfsense.cz/dns_records
+	echo "cli4 --post name='${NAME}' type=A content=\"${IPV4}\" /zones/:pfsense.cz/dns_records"
+	echo "-----------------------------------------------------------------------------------------------"
+	/usr/local/bin/cli4 --post name='${NAME}' type=A content="${IPV4}" /zones/:pfsense.cz/dns_records
 done
