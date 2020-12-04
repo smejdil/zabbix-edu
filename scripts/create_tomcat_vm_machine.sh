58fb45775ec90c0f30f448137d7097bbf9d9dc72
Lukas Maly <Iam@LukasMaly.NET>
2020-12-04 21:05:19 +0100

    Changes

--- scripts/create_tomcat_vm_machine.sh
+++ scripts/create_tomcat_vm_machine.sh
@@ -2,7 +2,7 @@
 #
 # Create GCP Linux server with Tomcat VM
 #
-# Lukas Maly <Iam@LukasMaly.NET> 11.11.2020
+# Lukas Maly <Iam@LukasMaly.NET> 4.12.2020
 #
 
 IMAGE_LIN=`gcloud compute images list | grep centos-7 | awk '{print $1}'`
@@ -12,11 +12,33 @@ argv=("$@");
 
 for (( j=0; j<argc; j++ )); do
     echo "linsrv${argv[j]}";
-    gcloud compute instances create linsrv${argv[j]} --image ${IMAGE_LIN} --image-project=centos-cloud  --metadata-from-file startup-script=./zabbix-edu/scripts/install-tomcat-server.sh
-    gcloud compute instances add-tags linsrv${argv[j]} --tags=http-server
-    gcloud compute instances add-tags linsrv${argv[j]} --tags=https-server
-    gcloud compute instances add-tags linsrv${argv[j]} --tags=zabbix-agent
-    gcloud compute instances add-tags linsrv${argv[j]} --tags=zabbix-server
+    gcloud compute instances create linsrv${argv[j]} --image ${IMAGE_LIN} --image-project=centos-cloud --zone=europe-west4-c --metadata-from-file startup-script=./zabbix-edu/scripts/install-tomcat-server.sh
+    gcloud compute instances add-tags linsrv${argv[j]} --tags=http-server --zone=europe-west4-c
+    gcloud compute instances add-tags linsrv${argv[j]} --tags=https-server --zone=europe-west4-c
+    gcloud compute instances add-tags linsrv${argv[j]} --tags=zabbix-agent --zone=europe-west4-c
+    gcloud compute instances add-tags linsrv${argv[j]} --tags=zabbix-server --zone=europe-west4-c
 done
+
+# gcloud compute regions list | grep europe-west
+
+# europe-west1-b		St. Ghislain, Belgium, Europe
+# europe-west1-c
+# europe-west1-d
+
+# europe-west2-a	London, England, Europe
+# europe-west2-b
+# europe-west2-c
+
+# europe-west3-a		Frankfurt, Germany Europe
+# europe-west3-b
+# europe-west3-c
+
+# europe-west4-a		Eemshaven, Netherlands, Europe
+# europe-west4-b
+# europe-west4-c
+
+# europe-west6-a		Zurich, Switzerland, Europe
+# europe-west6-b
+# europe-west6-c
 
 # EOF
--- scripts/create_zabbix_vm_machines.sh
+++ scripts/create_zabbix_vm_machines.sh
@@ -2,7 +2,7 @@
 #
 # Create GCP CentOS 8 VM
 #
-# Lukas Maly <Iam@LukasMaly.NET> 6.11.2020
+# Lukas Maly <Iam@LukasMaly.NET> 4.12.2020
 #
 
 IMAGE_CENTOS8=`gcloud compute images list | grep centos-8 | awk '{print $1}'`
@@ -12,11 +12,33 @@ argv=("$@");
 
 for (( j=0; j<argc; j++ )); do
     echo "zbx${argv[j]}";
-    gcloud compute instances create zbx${argv[j]} --image ${IMAGE_CENTOS8} --image-project=centos-cloud --metadata-from-file startup-script=./zabbix-edu/scripts/install-gcp.sh
-    gcloud compute instances add-tags zbx${argv[j]} --tags=http-server
-    gcloud compute instances add-tags zbx${argv[j]} --tags=https-server
-    gcloud compute instances add-tags zbx${argv[j]} --tags=zabbix-agent
-    gcloud compute instances add-tags zbx${argv[j]} --tags=zabbix-server
-done
+    gcloud compute instances create zbx${argv[j]} --image ${IMAGE_CENTOS8} --image-project=centos-cloud --zone=europe-west1-c --metadata-from-file startup-script=./zabbix-edu/scripts/install-gcp.sh
+    gcloud compute instances add-tags zbx${argv[j]} --tags=http-server --zone=europe-west1-c
+    gcloud compute instances add-tags zbx${argv[j]} --tags=https-server --zone=europe-west1-c
+    gcloud compute instances add-tags zbx${argv[j]} --tags=zabbix-agent --zone=europe-west1-c
+    gcloud compute instances add-tags zbx${argv[j]} --tags=zabbix-server --zone=europe-west1-c
+done 
+
+# gcloud compute regions list | grep europe-west
+
+# europe-west1-b		St. Ghislain, Belgium, Europe
+# europe-west1-c
+# europe-west1-d
+
+# europe-west2-a	London, England, Europe
+# europe-west2-b
+# europe-west2-c
+
+# europe-west3-a		Frankfurt, Germany Europe
+# europe-west3-b
+# europe-west3-c
+
+# europe-west4-a		Eemshaven, Netherlands, Europe
+# europe-west4-b
+# europe-west4-c
+
+# europe-west6-a		Zurich, Switzerland, Europe
+# europe-west6-b
+# europe-west6-c
 
 # EOF
