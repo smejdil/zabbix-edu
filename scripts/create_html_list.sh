f62f56cb3237420b4f7c8e2fc482e5368d650b40
Lukas Maly <Iam@LukasMaly.NET>
2020-12-06 12:22:17 +0100

    Changes

--- scripts/create_html_list.sh
+++ scripts/create_html_list.sh
@@ -7,9 +7,15 @@
 
 # ./zabbix-edu/scripts/create_html_list.sh
 
+GCP_CMD_LIST="gcloud compute instances list"
+
 echo "<html><body>" > ./zabbix-edu/files/vm_url_ipv4.html
-gcloud compute instances list | awk '{print $5" - <a href=\"http://"$1".pfsense.cz/zabbix/\">http://"$1".pfsense.cz/zabbix/</a><br>"}' | grep zbx0 >> ./zabbix-edu/files/vm_url_ipv4.html
-gcloud compute instances list | awk '{print $5" - <a href=\"http://"$1".pfsense.cz/\">http://"$1".pfsense.cz/</a><br>"}' | grep linsrv0 >> ./zabbix-edu/files/vm_url_ipv4.html
-gcloud compute instances list | awk '{print $5" - <a href=\"http://"$1".pfsense.cz/\">http://"$1".pfsense.cz/</a><br>"}' | grep winsrv0 >> ./zabbix-edu/files/vm_url_ipv4.html
-gcloud compute instances list | awk '{print $5" - <a href=\"http://"$1".pfsense.cz/\">http://"$1".pfsense.cz/</a><br>"}' | grep pfsense0 >> ./zabbix-edu/files/vm_url_ipv4.html
+echo "<ul >" >> ./zabbix-edu/files/vm_url_ipv4.html
+${GCP_CMD_LIST} | awk '{print "<li><a href=\"http://"$1".pfsense.cz/zabbix/\">http://"$1".pfsense.cz/zabbix/</a> - "$5"<br>"}' | grep zbx0 >> ./zabbix-edu/files/vm_url_ipv4.html
+${GCP_CMD_LIST} | awk '{print "<li><a href=\"http://"$1".pfsense.cz/\">http://"$1".pfsense.cz/</a> - "$5"<br>"}' | grep linsrv0 >> ./zabbix-edu/files/vm_url_ipv4.html
+${GCP_CMD_LIST} | awk '{print "<li><a href=\"http://"$1".pfsense.cz/\">http://"$1".pfsense.cz/</a> - "$5"<br>"}' | grep winsrv0 >> ./zabbix-edu/files/vm_url_ipv4.html
+${GCP_CMD_LIST} | awk '{print "<li><a href=\"http://"$1".pfsense.cz/\">http://"$1".pfsense.cz/</a> - "$5"<br>"}' | grep pfsense0 >> ./zabbix-edu/files/vm_url_ipv4.html
+echo "<ul>" >> ./zabbix-edu/files/vm_url_ipv4.html
 echo "</body></html>" >> ./zabbix-edu/files/vm_url_ipv4.html
+
+# EOF
