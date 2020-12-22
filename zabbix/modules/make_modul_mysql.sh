ebb98bc87e84f8a777c9764d5d538e3f97578723
Lukas Maly <smejdil@gmail.com>
2020-12-22 12:33:05 +0100

    Changes

--- zabbix/modules/make_modul_mysql.sh
+++ zabbix/modules/make_modul_mysql.sh
@@ -2,8 +2,9 @@
 #
 # Easy build shell script for Zabbix agent
 #
-# Lukas Maly <Iam@LukasMaly.NET> 6.11.2020
+# Lukas Maly <Iam@LukasMaly.NET> 22.12.2020
 #
+
 ZBX_VERSION=`rpm -qa | grep 'zabbix-agent-' | awk 'BEGIN{FS="-"}{print $3}'`
 ZBX_MAJOR_VERSION=`rpm -qa | grep 'zabbix-agent-' | awk 'BEGIN{FS="-"}{print $3}' | awk 'BEGIN{FS="."}{print $1}'`
 ZBX_MAJOR2_VERSION=`rpm -qa | grep 'zabbix-agent-' | awk 'BEGIN{FS="-"}{print $3}' | awk 'BEGIN{FS="."}{print $2}'`
