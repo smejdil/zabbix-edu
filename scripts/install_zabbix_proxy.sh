6147acc0d6f0b2db2ce9dd7534fe32e78fb77725
Lukas Maly <smejdil@gmail.com>
2021-11-10 20:24:05 +0100

    Fix install script for zabbix proxy

--- scripts/install_zabbix_proxy.sh
+++ scripts/install_zabbix_proxy.sh
@@ -15,15 +15,15 @@ ZABBIX_PROXY_MYSQL_PASS=`cat /root/mysql-zabbix-proxy.pw`
 MYSQL_ROOT_PASS=`cat /root/mysql-root.pw`
 
 # Create database and user
-mysql -u root -p${MYSQL_ROOT_PASS} -e "create database zabbix-proxy character set utf8 collate utf8_bin;"
-mysql -u root -p${MYSQL_ROOT_PASS} -e "grant all privileges on zabbix-proxy.* to zabbix-proxy@localhost identified by '${ZABBIX_PROXY_MYSQL_PASS}';"
+mysql -u root -p${MYSQL_ROOT_PASS} -e "create database zabbix_proxy character set utf8 collate utf8_bin;"
+mysql -u root -p${MYSQL_ROOT_PASS} -e "grant all privileges on zabbix_proxy.* to zabbix_proxy@localhost identified by '${ZABBIX_PROXY_MYSQL_PASS}';"
 mysql -u root -p${MYSQL_ROOT_PASS} -e "FLUSH PRIVILEGES;"
 
 ### Zabbix proxy
 
 # Import db schema
 echo "--- Import DB schema ---"
-zcat /usr/share/doc/zabbix-proxy-mysql/schema.sql.gz | mysql -u zabbix-proxy -p${ZABBIX_PROXY_MYSQL_PASS} zabbix-proxy
+zcat /usr/share/doc/zabbix-proxy-mysql/schema.sql.gz | mysql -u zabbix_proxy -p${ZABBIX_PROXY_MYSQL_PASS} zabbix_proxy
 
 # Zabbix configuration
 echo "--- Configure Zabbix proxy config ---"
@@ -31,8 +31,8 @@ cp -v /etc/zabbix/zabbix_proxy.conf /etc/zabbix/zabbix_proxy.conf-orig
 sed -i "s/^# ListenPort=10051/# ListenPort=10053/g" /etc/zabbix/zabbix_proxy.conf
 sed -i "s/Hostname=Zabbix proxy/Hostname=zbxp-local/g" /etc/zabbix/zabbix_proxy.conf
 sed -i "s/# DBHost=localhost/DBHost=localhost/g" /etc/zabbix/zabbix_proxy.conf
-sed -i "s/^DBName=zabbix_proxy/DBHost=zabbix-proxy/g" /etc/zabbix/zabbix_proxy.conf
-sed -i "s/^DBUser=zabbix/DBUser=zabbix-proxy/g" /etc/zabbix/zabbix_proxy.conf
+sed -i "s/^DBName=zabbix_proxy/DBHost=zabbix_proxy/g" /etc/zabbix/zabbix_proxy.conf
+sed -i "s/^DBUser=zabbix/DBUser=zabbix_proxy/g" /etc/zabbix/zabbix_proxy.conf
 sed -i "s|^# DBPassword=|DBPassword=${ZABBIX_PROXY_MYSQL_PASS}|g" /etc/zabbix/zabbix_proxy.conf
 sed -i "s/# Timeout=3/Timeout=30/g" /etc/zabbix/zabbix_proxy.conf
 sed -i "s/# CacheSize=8M/CacheSize=32M/g" /etc/zabbix/zabbix_proxy.conf
