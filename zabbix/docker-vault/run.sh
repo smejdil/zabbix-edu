f080a3083e6cfd39a1bded1efdd393f2db51e866
Lukas Maly <smejdil@gmail.com>
2021-11-19 10:48:45 +0100

    Fix vault kv zabbix

--- zabbix/docker-vault/run.sh
+++ zabbix/docker-vault/run.sh
@@ -10,12 +10,12 @@ done
 echo "Authenticating to vault..."
 vault login token=DataScript-ZabbixEDU202X
 
-echo "Initializing vault..."
-vault secrets enable -version=2 -path=zabbix kv
+#echo "Initializing vault..."
+#vault secrets enable -version=2 -path=zabbix kv
 
 echo "Adding entries..."
-vault kv put zabbix ZBX_PROBE_MYSQL_PASS=ASce6qQA9gtyWiGnGpmj
-vault kv put zabbix SSH_USER_PASS=8VsnE5QiYexIu4kazcBI
-vault kv put zabbix ZBX_PROBE_VMWARE_PASS=8VsnE5QiYexIu4kazcBI
+vault kv put secret/zabbix ZBX_PROBE_MYSQL_PASS=ASce6qQA9gtyWiGnGpmj \
+			 SSH_USER_PASS=8VsnE5QiYexIu4kazcBI \
+			 ZBX_PROBE_VMWARE_PASS=8VsnE5QiYexIu4kazcBI
 
 echo "Complete..."
