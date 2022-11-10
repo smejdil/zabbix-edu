63913884a4f62dc2c50f0446ebb3bd8ebbb58051
Lukas Maly <smejdil@gmail.com>
2022-11-10 19:25:39 +0100

    Fix problem in script

--- scripts/install-freebsd-server.sh
+++ scripts/install-freebsd-server.sh
@@ -5,19 +5,24 @@
 # Lukas Maly <Iam@LukasMaly.NET> 9.11.2022
 #
 
-pkg -y install joe mc git
+pkg -y install  -y joe mc git
 
 echo "BATCH=yes" > /etc/make.conf
 
-portsnap fetch && portsnap extract
+portsnap fetch
+portsnap extract
+
+cd /usr/ports/devel/p5-JSON-RPC && make install clean
 
 cd /usr/ports/devel/py-pip && make install clean
+
 cd /usr/ports/sysutils/ansible && make install clean
+
 pip install zabbix-api
 
 # Zabbix Agent !!!
 
 cd /root
-git clone https://github.com/smejdil/zabbix-ansible
+git clone https://github.com/smejdil/zabbix-api
 
 # EOF
