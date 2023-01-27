ca5ae101b3715c403131fbc0e08d86db86fcab10
Lukas Maly <smejdil@gmail.com>
2023-01-27 16:24:02 +0100

    Fix zabbix install packages + epel

--- scripts/install-gcp.sh
+++ scripts/install-gcp.sh
@@ -9,10 +9,14 @@
 dnf -y install git
 dnf -y install bash-completion
 dnf -y install epel-release
+cp /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo-orig
+sed -i 's/# place its address here./excludepkgs=zabbix*/g' /etc/yum.repos.d/epel.repo
 dnf -y install joe vim mc wget
 dnf -y install bind-utils
 dnf -y install gcc pcre-devel make libtool autoconf automake dbus-devel
-dnf -y install mariadb-server mariadb-devel mariadb-connector-odbc
+dnf -y install mariadb-server 
+#dnf -y install mariadb-devel 
+dnf -y install mariadb-connector-odbc
 dnf -y install postgresql-odbc
 dnf -y install libiodbc
 dnf -y install systemd-devel
@@ -21,7 +25,16 @@ dnf -y install net-snmp net-snmp-libs net-snmp-utils
 rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/9/x86_64/zabbix-release-6.0-4.el9.noarch.rpm
 dnf clean all
 dnf -y install ipmitool
-dnf -y install zabbix-server-mysql zabbix-web-mysql zabbix-apache-conf zabbix-sql-scripts zabbix-selinux-policy zabbix-agent zabbix-agent2 zabbix-sender zabbix-get zabbix-java-gateway zabbix-js
+dnf -y install zabbix-server-mysql 
+dnf -y install zabbix-web-mysql 
+dnf -y install zabbix-apache-conf
+dnf -y install zabbix-sql-scripts 
+dnf -y install zabbix-selinux-policy 
+dnf -y install zabbix-agent 
+dnf -y install zabbix-agent2 
+dnf -y install zabbix-sender 
+dnf -y install zabbix-get
+dnf -y install zabbix-js
 dnf -y install perl-App-cpanminus
 dnf -y install perl-JSON.noarch
 dnf -y install ansible-core pip
