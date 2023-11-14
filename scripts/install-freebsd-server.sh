7a8ef35293963853209ab5546cf4a671f3c0ea13
Lukas Maly <smejdil@gmail.com>
2023-11-15 00:16:20 +0100

    Fix install-freebsd-server.sh

--- scripts/install-freebsd-server.sh
+++ scripts/install-freebsd-server.sh
@@ -27,31 +27,23 @@ echo "BATCH=yes" > /etc/make.conf
 echo "ALLOW_UNSUPPORTED_SYSTEM=yes" >> /etc/make.conf
 
 # update ports
+pkg install -y portsnap portupgrade
 portsnap fetch
 portsnap extract
 
+# install ports
 cd /usr/ports/devel/p5-JSON-RPC && make install clean
 cd /usr/ports/www/p5-LWP-Protocol-https && make install clean
 
-pkg install -y portsnap portupgrade
 pkg install -y py39-lxml py39-ansible jq
 cd /usr/ports/devel/py-pip && make install clean
 cd /usr/ports/net-mgmt/py-pyzabbix && make install clean
 
 pip install zabbix-api
 
+# Zabbix agentd
 pkg install -y zabbix6-agent
 /usr/local/etc/rc.d/zabbix_agentd enable
-# sed edit config
-
-cd /root
-git clone https://github.com/smejdil/zabbix-api
-
-#cd /usr/ports/lang/lua54 && make install clean
-#cd /usr/ports/converters/lua-json && make install clean
-#cd /usr/ports/net/luasocket && make install clean
-
-# Zabbix agentd
 cp -v /usr/local/etc/zabbix6/zabbix_agentd.conf /usr/local/etc/zabbix6/zabbix_agentd.conf-orig
 gsed -i 's/Hostname=Zabbix server/#Hostname=Zabbix server/g' /usr/local/etc/zabbix6/zabbix_agentd.conf
 gsed -i 's/Server=127.0.0.1/Server=127.0.0.1,enceladus.pfsense.cz/g' /usr/local/etc/zabbix6/zabbix_agentd.conf
@@ -59,5 +51,9 @@ gsed -i 's/# LogRemoteCommands=0/LogRemoteCommands=1/g' /usr/local/etc/zabbix6/z
 gsed -i 's/# DenyKey=system.run\[\*\]/AllowKey=system.run\[\*\]/g' /usr/local/etc/zabbix6/zabbix_agentd.conf
 diff -u /usr/local/etc/zabbix6/zabbix_agentd.conf-orig /usr/local/etc/zabbix6/zabbix_agentd.conf
 /usr/local/etc/rc.d/zabbix_agentd restart
+
+# project zabbix-api
+cd /root
+git clone https://github.com/smejdil/zabbix-api
 
 # EOF
