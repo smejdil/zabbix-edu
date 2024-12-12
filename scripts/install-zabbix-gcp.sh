e8b0413449911d5d237773173088293c5ca581f7
Lukas Maly <smejdil@gmail.com>
2024-12-12 20:24:15 +0100

    Update Zabbix 7.0 LTS + Ansible Zabbix instalation

--- scripts/install-zabbix-gcp.sh
+++ scripts/install-zabbix-gcp.sh
@@ -11,24 +11,12 @@ apt install -y git
 apt install -y bash-completion
 apt install -y joe vim mc wget
 apt install -y ansible
-
-#dnf -y install bind-utils
-#dnf -y install gcc pcre-devel make libtool autoconf automake dbus-devel
-#dnf -y install mariadb-server 
-#dnf -y install mariadb-devel 
-#dnf -y install mariadb-connector-odbc
-#dnf -y install mariadb-connector-c-devel.x86_64
-#dnf -y install postgresql-odbc
-#dnf -y install libiodbc
-#dnf -y install systemd-devel
-#dnf -y install python3-systemd
-#dnf -y install net-snmp net-snmp-libs net-snmp-utils
-#rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/9/x86_64/zabbix-release-6.0-4.el9.noarch.rpm
-#dnf clean all
-#dnf -y install ipmitool
-#dnf -y install perl-JSON.noarch
-#dnf -y install ansible-core pip
-#dnf -y install golang-github-prometheus-node-exporter
+apt install -y ipmitool
+apt install -y aptitude
+apt install -y odbc-mariadb
+apt install -y snmp
+apt install -y python3-pip
+apt install -y prometheus-node-exporter
 
 # Get repo
 cd /root/ && git clone https://github.com/smejdil/zabbix-edu
@@ -47,6 +35,6 @@ ansible-playbook /root/zabbix-edu/zabbix/ansible/add-host-group-training.yml
 ansible-playbook /root/zabbix-edu/zabbix/ansible/add-user-zbx_probe.yml
 ansible-playbook /root/zabbix-edu/zabbix/ansible/add-template-training.yml
 
-echo "Done" > /root/done.txt
+echo "Done" > /root/Done
 
 # EOF
